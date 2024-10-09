1f4409e2c5024adc84d919a11b29d60d84336408
ubun2 <officiumrflss@gmail.com>
2024-10-09 19:32:59 -0300

    My fix for the Fibonacci assignment

--- fib.js
+++ fib.js
@@ -1,16 +1,16 @@
 function fib(run_num) {
-    let fib_seq = [0,1]
 
     if (run_num > 0) {
-        for (let i = fib_seq.length; i < run_num; i++) {
-            fib_seq[i] = fib_seq[i - 1] + fib_seq[i - 2]            
 
+        let fib_seq = [0,1]
+
+        for (let i = 2; i < run_num; i++) {
+            fib_seq[i] = fib_seq[i - 1] + fib_seq[i - 2];
         }
 
+        return fib_seq
     }
-
-    return fib_seq
 
 }
 
-console.log(fib(8));
+console.log(fib(21));
