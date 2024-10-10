2ef9bdcc0029fb399c96a83d9cdae4889c9a90a8
ubun2 <officiumrflss@gmail.com>
2024-10-10 12:05:06 -0300

    Last merge sort addition

--- sort.js
+++ sort.js
@@ -14,4 +14,34 @@ function sort(arr) {
     return arr
 }
 
-console.log(sort([100,1,8,7,2,91,19,10]));
+function mergesort(arr) {
+    if (arr.length <= 1) return arr;
+
+    const half = Math.floor(arr.length / 2);
+    const left = mergesort(arr.slice(0, half));
+    const right = mergesort(arr.slice(half));
+
+    return merge(left, right)
+}
+
+function merge(left, right) {
+  let sort = [] 
+  let i = 0; j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+        sort.push(left[i]);
+        i++;
+    } else {
+        sort.push(right[j]);
+        j++;
+    }
+  }
+
+  return sort.concat(left.slice(i)).concat(right.slice(j));
+    
+}
+
+console.log(mergesort([5,9,8,7,4,3,2,1]))
+
+
