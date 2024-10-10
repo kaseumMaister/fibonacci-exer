f166364e300e69327a771d1638e5417731c40f93
ubun2 <officiumrflss@gmail.com>
2024-10-09 23:51:49 -0300

    Fibonacci Sequence now in recursive function

--- fib.js
+++ fib.js
@@ -4,7 +4,7 @@ function fib(run_num) {
 
         let fib_seq = [0,1]
 
-        for (let i = 2; i < run_num; i++) {
+        for (let i = 2; i <= run_num; i++) {
             fib_seq[i] = fib_seq[i - 1] + fib_seq[i - 2];
         }
 
@@ -13,4 +13,15 @@ function fib(run_num) {
 
 }
 
-console.log(fib(21));
+function fibsRec(run) {
+    if (run === 0) return [0];
+    if (run === 1) return [0, 1];
+    else {
+        let fib_seq = fibsRec(run - 1);
+        fib_seq.push(fib_seq[run - 1] + fib_seq[run - 2]);
+        return fib_seq
+    }
+}
+console.log(fib(8));
+
+console.log(fibsRec(8));
